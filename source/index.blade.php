cc062a79d31a018a3d3a228e37e64c2127fbceba
mrpoth <adam.pothitos@gmail.com>
2019-09-16 21:28:08 +0100

    Added resume

--- source/index.blade.php
+++ source/index.blade.php
@@ -93,8 +93,12 @@
       <p>Built by Adam Pothitos with Jigsaw, hosted on Netlify</p>
       <div class="social-container">
     <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
-    ><a href="https://www.linkedin.com/in/adam-pothitos-378154145/">LinkedIn</a>
-        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"><a href="https://github.com/mrpoth/">GitHub</a>
+    >
+      <a href="https://www.linkedin.com/in/adam-pothitos-378154145/">LinkedIn</a>
+    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png">
+      <a href="https://github.com/mrpoth/">GitHub</a>
+    <img src="https://cdn.pixabay.com/photo/2017/03/24/02/25/resume-2169945_960_720.png">
+      <a href="https://drive.google.com/file/d/17uPe2Kp93EGPbr27AQ1D6ybu1EexGcJH/view?usp=sharing">Resume</a>
 </div>
   </div>
 
