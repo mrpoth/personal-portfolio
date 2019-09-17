9fa0385f727cdd5d40febe2bf42ca8fd4670c5d9
mrpoth <adam.pothitos@gmail.com>
2019-09-17 06:21:25 +0100

    fixed contact links

--- source/index.blade.php
+++ source/index.blade.php
@@ -19,7 +19,7 @@
     <li><a href="#about">About</a></li>
     <li><a href="#skills">Skills & Tech</a></li>
     <li><a href="#projects">Projects</a></li>
-    <li><a href="#">Contact</a></li>
+    <li><a href="#contact">Contact</a></li>
   </ul>
 </nav>
 </a>
@@ -102,12 +102,12 @@
   <div id="footer">
       <p>Built by Adam Pothitos with Jigsaw, hosted on Netlify</p>
       <div class="social-container">
-    <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
+    <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png" width="20" height="20"
     >
       <a href="https://www.linkedin.com/in/adam-pothitos-378154145/">LinkedIn</a>
-    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png">
+    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"  width="20" height="20">
       <a href="https://github.com/mrpoth/">GitHub</a>
-    <img src="https://cdn.pixabay.com/photo/2017/03/24/02/25/resume-2169945_960_720.png">
+    <img src="https://cdn.pixabay.com/photo/2017/03/24/02/25/resume-2169945_960_720.png"  width="20" height="20">
       <a href="https://drive.google.com/file/d/17uPe2Kp93EGPbr27AQ1D6ybu1EexGcJH/view?usp=sharing">Resume</a>
 </div>
   </div>
